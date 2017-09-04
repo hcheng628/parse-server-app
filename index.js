50e4be01145a951e1c6b8727b9ca727892c07ba4
superhcheng <chenghongyu628@gmail.com>
2017-09-04 17:15:59 -0400

    add email verification, reset password .....

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ var api = new ParseServer({
 	databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
 	cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
 	serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-	
+
 	//**** Security Settings ****//
-	// allowClientClassCreation: process.env.CLIENT_CLASS_CREATION || false, 
+	// allowClientClassCreation: process.env.CLIENT_CLASS_CREATION || false,
 	appId: process.env.APP_ID || 'myAppId',
-	masterKey: process.env.MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!	
-	
+	masterKey: process.env.MASTER_KEY || 'myMasterKey', //Add your master key here. Keep it secret!
+
 	//**** Live Query ****//
 	// liveQuery: {
 	// 	classNames: ["TestObject", "Place", "Team", "Player", "ChatMessage"] // List of classes to support for query subscriptions
@@ -28,23 +28,23 @@ var api = new ParseServer({
 
 	//**** Email Verification ****//
 	/* Enable email verification */
-	// verifyUserEmails: true,
+	verifyUserEmails: true,
 	/* The public URL of your app */
 	// This will appear in the link that is used to verify email addresses and reset passwords.
 	/* Set the mount path as it is in serverURL */
-	// publicServerURL: process.env.SERVER_URL || 'http://localhost:1337/parse',
+	publicServerURL: process.env.SERVER_URL || 'http://localhost:1337/parse',
 	/* This will appear in the subject and body of the emails that are sent */
-	// appName: process.env.APP_NAME || "MyApp", 
+	appName: process.env.APP_NAME || "MyApp",
 
-	// emailAdapter: {
-	// 	module: 'parse-server-simple-mailgun-adapter',
-	// 	options: {
-	// 		fromAddress: process.env.EMAIL_FROM || "test@example.com",
-	// 		domain: process.env.MAILGUN_DOMAIN || "example.com",
-	// 		apiKey: process.env.MAILGUN_API_KEY  || "apikey"
-	// 	}
-	// },
-	
+	emailAdapter: {
+		module: 'parse-server-simple-mailgun-adapter',
+		options: {
+			fromAddress: process.env.EMAIL_FROM || "test@example.com",
+			domain: process.env.MAILGUN_DOMAIN || "example.com",
+			apiKey: process.env.MAILGUN_API_KEY  || "apikey"
+		}
+	},
+
 	//**** File Storage ****//
 	// filesAdapter: new S3Adapter(
 	// 	{
