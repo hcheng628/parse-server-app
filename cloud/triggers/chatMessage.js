836dcfac8260517ba70b80e5d864a6b167bd1371
supercheng <chenghongyu628@gmail.com>
2017-09-24 20:48:10 -0400

    add chat message valid check

---
+++ cloud/triggers/chatMessage.js
@@ -0,0 +1,5 @@
+Parse.Cloud.beforeSave("ChatMessage", function(request, response) {
+    if (!request.object.get("name") || request.object.get('content')) {
+        response.error("Both username and content cannot be empty!");
+    }
+});
