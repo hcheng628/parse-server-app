f806405452f00bda55c675b255f83786971dba0b
supercheng <chenghongyu628@gmail.com>
2017-09-24 21:09:39 -0400

    add bot chat feature

--- cloud/triggers/chatMessage.js
+++ cloud/triggers/chatMessage.js
@@ -1,5 +1,20 @@
 Parse.Cloud.beforeSave("ChatMessage", function(request, response) {
-    if (!request.object.get("name") || request.object.get('content')) {
+    var inMsg = request.object.get('content');
+    var inUsername = request.object.get("name");
+
+    if (!inUsername || !inMsg) {
         response.error("Both username and content cannot be empty!");
+    } else {
+        if(newMsg.toLowerCase().indexOf("@bot") >= 0){
+            var ChatMessage = Parse.Object.extend("ChatMessage");
+            var message = new ChatMessage();
+            message.set("name", 'bot');
+            message.set("content", 'Hia @' + inUsername + 'how can I help you?');
+            message.save().then(function(obj) {
+                console.log("Message saved!");
+            }, function(err) {
+                alert(err.message);
+            });
+        }
     }
 });
